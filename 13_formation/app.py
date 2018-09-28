16650195012199f4cc755967c202143771c350d8
Your Name <you@example.com>
2018-09-28 12:35:11 -0400

    9-28 cw

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -10,11 +10,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def landingPage():
+    print(request.method)
     return render_template('home.html')
 
-@app.route("/auth")
+@app.route("/auth", methods=["POST", "GET"])
 def authenticate():
-    return render_template('auth.html', un=request.args['username'])
+    
+    return render_template('auth.html', un=request.args.get("username"))
 
 app.debug = True
 app.run()
