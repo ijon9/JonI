b017e0412de4277e77a6ec8465f080b73896828b
unknown <ijon@stuy.edu>
2018-09-27 21:33:42 -0400

    k13

---
+++ 13_formation/app.py
@@ -0,0 +1,20 @@
+#Isaac Jon
+#SoftDev1 pd6
+#K13 -- Echo Echo Echo
+#2018-09-28
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def landingPage():
+    return render_template('home.html')
+
+@app.route("/auth")
+def authenticate():
+    return render_template('auth.html', un=request.args['username'])
+
+app.debug = True
+app.run()
