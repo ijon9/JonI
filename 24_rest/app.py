1a59fd2de3bc9a01bdf6944288c657938188750d
unknown <ijon@stuy.edu>
2018-11-13 21:31:08 -0500

    hw24

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+import urllib, json
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return render_template(api="https://api.nasa.gov/planetary/apod?api_key=qJF4djMIqZSBeU81jvbqpKKYerJuVO0Y7hZ7pwXV",'templates/index.html')
+    api = json.loads(urllib.request.urlopen('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-11-13&cloud_score=True&api_key=qJF4djMIqZSBeU81jvbqpKKYerJuVO0Y7hZ7pwXV').read())
+    print(api)
+    return render_template('index.html', image=api['url'])
 
-app.run()
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
