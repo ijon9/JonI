7147f3e1eb021931dfc7b03a7b4f876640b1b43d
ijon9 <ijon@stuy.edu>
2018-12-19 12:01:52 -0500

    hw29

---
+++ 29_js/funky_town.js
@@ -0,0 +1,33 @@
+//Team [] -- Isaac Jon and Mohammed Uddin
+//SoftDev1 pd6
+//K#28 -- Sequential Progression
+//2018-12-19
+
+var fibonacci = function(n) {
+  if(n == 0)
+    return 0;
+  if(n < 2)
+    return 1;
+  else
+    return fibonacci(n-2) + fibonacci(n-1);
+};
+
+var gcd = function(a, b) {
+  if(a == 0)
+    return b;
+  return gcd(b%a, a);
+};
+
+var listOfStudents = ["George", "Bob", "Nina", "Elizabeth", "John", "Charlie"];
+
+var randomStudent = function() {
+  randNum = parseInt((Math.random() * listOfStudents.length))
+  return console.log(listOfStudents[randNum]);
+};
+
+f = document.getElementById('fib');
+g = document.getElementById('gcd');
+r = document.getElementById('randStu');
+
+f.addEventListener('click', fibonacci);
+r.addEventListener('click', randomStudent);
